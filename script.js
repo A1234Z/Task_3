b083705eac79f1896fbc3e613e14075dd9f50b2f
Rajula Asritha <91464401+A1234Z@users.noreply.github.com>
2023-05-21 22:01:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Add interactivity if desired
+// For example, you can add animations, tooltips, or interactive elements using JavaScript libraries like jQuery or D3.js
+// Here's an example using jQuery to fade in the infographic on page load:
+
+$(document).ready(function() {
+  $('#infographic').fadeIn(1000);
+});
